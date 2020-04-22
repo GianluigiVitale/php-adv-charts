ca8dd297ca221a84bc30e705ce981e026f4a5966
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-04-22 16:04:39 +0200

    Added 4 more methods to deal with the data to create the chart
    -Method 1: data inside the script
    -Method 2: take the data from database and convert it to json format
    -Method 3: take the data from php with json format
    -Method 4: create a data-database inside the canvas and save the data in a variable thanks to jQuery

--- index.php
+++ index.php
@@ -13,6 +13,39 @@
             <canvas id="sales-month"></canvas>
         </div>
 
+
+
+        <!-- Alternative methods (really really ugly) -->
+            <!-- <div class="container">                                                                // method 4
+                < ?php include 'database.php' ?>
+                <canvas id="sales-month" data-database="< ?php echo $database ?>"></canvas>
+            </div> -->
+            <script type="text/javascript">
+            /*
+                < ?php // $data = [1000,1322,1123,2301,3288,988,502,2300,5332,2300,1233,2322]; ?>  // method 1
+                < ?php include 'database.php' ?>                                                     // method 2
+                // var dati = < ?php echo json_encode($data) ?>                                      // method 2
+                // var dati = < ?php echo $database ?>                                                  // method 3
+
+                // var dati = $('#sales-month').data('database');                                           // method 4
+                var ctx = $('#sales-month');
+                var chart = new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],
+                        datasets: [{
+                            label: 'Monthly Sales',
+                            backgroundColor: '#2E4691',
+                            borderColor: '#2E4691',
+                            data: dati
+                        }]
+                    },
+                });
+            */
+            </script>
+        <!-- end bad methods -->
+
+
         <script src="js/main.js" charset="utf-8"></script>
     </body>
 </html>
